c57d7a9acaddbc82ae388cc414dd46674b76b86e
Nematulloh <unn.info.tech@gmail.com>
2025-12-04 12:15:07 +0500

    Initial commit: Django project with static files and tests

---
+++ main/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_homepage_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, "Hello, Django!")
