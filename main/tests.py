0ba7a20ee3af963e70aa9bfaaefcf2cb9b94a0ce
Nematulloh <unn.info.tech@gmail.com>
2025-12-04 12:20:46 +0500

    Add GitHub Actions CI workflow

--- main/tests.py
+++ main/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     def test_homepage_status(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 40400)
+        self.assertEqual(response.status_code, 200)
 
     def test_homepage_content(self):
         response = self.client.get('/')
